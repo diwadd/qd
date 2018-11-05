e9432a32dd08b9701bfd0c5e27182187ed91293b
Dawid Dul <dawid.dul@gmail.com>
2018-11-05 20:43:14 +0100

    Saving drawing to npy format.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 import glob
 
 import ndjson
@@ -10,6 +11,7 @@ import support_functions as sf
 
 SIMPLIFIED_DATA_IMAGE_SIZE = 256
 REDUCED_DATA_IMAGE_SIZE = 28
+
 
 def get_data_files():
     data_files = glob.glob("data/*ndjson")
@@ -38,61 +40,54 @@ def convert_ndjson_image_to_numpy_array(ndjson_drawing):
             for p in points:
                 np_drawing[p[0]][p[1]] = 1
 
-
-
     # plt.matshow(np_drawing)
     # plt.show()
 
     return np_drawing
 
 
-def convert_images_from_ndjson_file_into_numpy_arrays(ndjson_file):
+def convert_images_from_ndjson_file_into_numpy_arrays_and_save(ndjson_file):
 
     with open(ndjson_file) as f:
         data = ndjson.load(f)
 
+    rc = re.compile(r"data/full%2Fsimplified%2F(?P<drawing_name>.*).ndjson")
+
+    rm = rc.match(ndjson_file)
+    logger.info("ndjson_file: {0}".format(ndjson_file))
+    logger.info("rm status: {0}".format(rm))
+
+    drawing_name = rm.group("drawing_name").replace(" ", "_")
+
     n_drawings = len(data)
     logger.info("Number of drawings = {0}".format(n_drawings))
 
-    np_drawing_array = np.zeros((n_drawings, REDUCED_DATA_IMAGE_SIZE, REDUCED_DATA_IMAGE_SIZE))
-
-    logger.debug("np_drawing_array size: {0}".format(np_drawing_array.shape))
-    for i in range(n_drawings):
+    for i in range(30):
         logger.info("Processing: {0}/{1}".format(i, n_drawings))
+        logger.info("found drawing_name: {0}".format(rm.group("drawing_name")))
         ndjson_drawing = data[i]["drawing"]
 
         np_drawing = convert_ndjson_image_to_numpy_array(ndjson_drawing)
 
         np_drawing = cv.resize(np_drawing,
-                               (REDUCED_DATA_IMAGE_SIZE, REDUCED_DATA_IMAGE_SIZE),
-                               interpolation = cv.INTER_AREA)
+                              (REDUCED_DATA_IMAGE_SIZE, REDUCED_DATA_IMAGE_SIZE),
+                              interpolation = cv.INTER_AREA)
 
         # plt.matshow(np_drawing)
         # plt.show()
 
-        np_drawing_array[i, :, :] = np_drawing
-
-    return np_drawing_array
+        output_file_name = "data/class_{0}_{1}x{2}_id_{3}.npy".format(drawing_name,
+                                                                             REDUCED_DATA_IMAGE_SIZE,
+                                                                             REDUCED_DATA_IMAGE_SIZE,
+                                                                             i)
+        np.save(output_file_name, np_drawing)
 
 
 def convert_ndjson_simplified_data_into_numpy_arrays(ndjson_file_list):
-
-    # First du,,y element so we have something to concatenate with.
-    np_reduced_drawings = np.zeros((1, REDUCED_DATA_IMAGE_SIZE, REDUCED_DATA_IMAGE_SIZE))
-
     n_files = len(ndjson_file_list)
     for i in range(n_files):
+        convert_images_from_ndjson_file_into_numpy_arrays_and_save(ndjson_file_list[i])
 
-        np_drawing_array = convert_images_from_ndjson_file_into_numpy_arrays(ndjson_file_list[i])
-        np_reduced_drawings = np.concatenate((np_reduced_drawings, np_drawing_array), axis=0)
-
-    logger.debug("Shape of np_reduced_drawings before droping dummy element: {0}".format(np_reduced_drawings.shape))
-
-    # Drop the first dummy element.
-    np_reduced_drawings = np_reduced_drawings[1:, :, :]
-    logger.debug("Shape of np_reduced_drawings after dropping dummy element: {0}".format(np_reduced_drawings.shape))
-
-    return np_reduced_drawings
 
 if __name__ == "__main__":
     print("Starting...")
@@ -101,7 +96,7 @@ if __name__ == "__main__":
 
     convert_ndjson_simplified_data_into_numpy_arrays(ndjson_file_list)
 
-    #np_drawing_array = convert_images_from_ndjson_file_into_numpy_arrays(data_files[1])
+    # np_drawing_array = convert_images_from_ndjson_file_into_numpy_arrays(data_files[1])
 
     # data = None
     # with open("data/full%2Fsimplified%2Faxe.ndjson") as f:
