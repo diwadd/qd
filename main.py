741fd876b4fc6a3fe8efe50904b5de506e796b11
Dawid Dul <dawid.dul@gmail.com>
2018-11-06 21:22:09 +0100

    Label encoding and train/test split

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 import re
 import glob
+import random
 
 import ndjson
 import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
 
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+
 from custom_logger import logger
 import support_functions as sf
 
+MAIN_SEED = 0
 SIMPLIFIED_DATA_IMAGE_SIZE = 256
 REDUCED_DATA_IMAGE_SIZE = 28
 
+random.seed(MAIN_SEED)
 
 def get_data_files():
     data_files = glob.glob("data/*ndjson")
     return data_files
 
+def get_numpy_drawings_list():
+    numpy_drawings_list = glob.glob("data/*.npy")
+    return numpy_drawings_list
 
 def convert_ndjson_image_to_numpy_array(ndjson_drawing):
-
     np_drawing = np.zeros((SIMPLIFIED_DATA_IMAGE_SIZE, SIMPLIFIED_DATA_IMAGE_SIZE))
 
     logger.debug("---> ndjson drawing START <---")
@@ -62,9 +70,11 @@ def convert_images_from_ndjson_file_into_numpy_arrays_and_save(ndjson_file):
     n_drawings = len(data)
     logger.info("Number of drawings = {0}".format(n_drawings))
 
-    for i in range(30):
-        logger.info("Processing: {0}/{1}".format(i, n_drawings))
-        logger.info("found drawing_name: {0}".format(rm.group("drawing_name")))
+    for i in range(n_drawings):
+        if i % 1000 == 0:
+            logger.info("Processing: {0}/{1}".format(i, n_drawings))
+
+        logger.debug("found drawing_name: {0}".format(rm.group("drawing_name")))
         ndjson_drawing = data[i]["drawing"]
 
         np_drawing = convert_ndjson_image_to_numpy_array(ndjson_drawing)
@@ -88,54 +98,77 @@ def convert_ndjson_simplified_data_into_numpy_arrays(ndjson_file_list):
     for i in range(n_files):
         convert_images_from_ndjson_file_into_numpy_arrays_and_save(ndjson_file_list[i])
 
+def get_labels(numpy_drawings_list):
+
+    rc = re.compile(r"data/class_(?P<drawing_name>.*)_\d+x\d+_id_\d+.npy")
+    n = len(numpy_drawings_list)
+
+    count_labels = {}
+    labels = [None for i in range(n)]
+    for i in range(n):
+            rm = rc.match(numpy_drawings_list[i])
+            assert rm, "Regexp not matched!"
+
+            l = rm.group("drawing_name")
+            labels[i] = l
+
+            if l in count_labels:
+                count_labels[l] = count_labels[l] + 1
+            else:
+                count_labels[l] = 0
+
+    logger.info("---   Histogram of labels   ---")
+    for key, value in sorted(count_labels.items()):
+        logger.info("{0:10s}: {1}".format(key, value))
+    logger.info("---   -------------------   ---")
+
+    return labels
+
+def split_the_numpy_drawings_into_test_train_evaluate_datasets():
+
+    numpy_drawings_list = get_numpy_drawings_list()
+    logger.debug("numpy_drawings_list length: {0}".format(len(numpy_drawings_list)))
+    logger.debug("Before shuffle")
+    logger.debug(numpy_drawings_list)
+
+    random.shuffle(numpy_drawings_list)
+    logger.debug("After shuffle")
+    logger.debug(numpy_drawings_list)
+
+    labels = get_labels(numpy_drawings_list)
+
+    for i in range(len(labels)):
+        logger.debug("{0}  -  {1}".format(numpy_drawings_list[i], labels[i]))
+
+    le = LabelEncoder()
+    le.fit_transform(labels)
+
+    logger.debug(le.transform(["axe", "bat", "baseball_bat"]))
+
+    logger.debug(labels)
+
+    labels = le.transform(labels)
+    logger.debug(labels)
+
+    x_train, x_test, y_train, y_test = train_test_split(numpy_drawings_list,
+                                                        labels,
+                                                        test_size=0.33,
+                                                        random_state=MAIN_SEED)
+
+    logger.debug("x_train: {0}".format(x_train))
+    logger.debug("y_train: {0}".format(y_train))
+
+    logger.debug("x_test: {0}".format(x_test))
+    logger.debug("y_test: {0}".format(y_test))
+
 
 if __name__ == "__main__":
     print("Starting...")
 
     ndjson_file_list = get_data_files()
 
+    # split_the_numpy_drawings_into_test_train_evaluate_datasets()
+
+    # Open each ndjson file, convert the drawings in it into numpy arrays
+    # and save them a *.npy files for further processing.
     convert_ndjson_simplified_data_into_numpy_arrays(ndjson_file_list)
-
-    # np_drawing_array = convert_images_from_ndjson_file_into_numpy_arrays(data_files[1])
-
-    # data = None
-    # with open("data/full%2Fsimplified%2Faxe.ndjson") as f:
-    #     data = ndjson.load(f)
-    #
-    # ndjson_drawing = data[243]["drawing"]
-    # convert_ndjson_image_to_numpy_array(ndjson_drawing)
-    #
-    #
-    # m_drawing = np.zeros((256, 256))
-    # print(m_drawing.shape)
-    # print(type(data))
-    #
-    # drawing = data[243]["drawing"]
-    #
-    # print(drawing)
-    # print("len: {0}".format(len(drawing)))
-    # # print(np.array(drawing))
-    # print(drawing[0])
-    # print(len(drawing[0]))
-    # print(drawing[0][1])
-    # print(len(drawing[0][1]))
-    #
-    # print(drawing[0][1][0])
-    #
-    # for i in range(len(drawing)):
-    #         for k in range(1, len(drawing[i][0])):
-    #             print("drawing[i][0][k]: " + str(drawing[i][0][k]))
-    #             print("drawing[i][1][k]: " + str(drawing[i][1][k]))
-    #
-    #             start = (drawing[i][0][k-1], drawing[i][1][k-1])
-    #             end = (drawing[i][0][k], drawing[i][1][k])
-    #
-    #             points = sf.get_line(start, end)
-    #             print("points: " + str(points))
-    #
-    #             for p in points:
-    #                 m_drawing[p[0]][p[1]] = 1
-    #
-    #
-    # plt.matshow(m_drawing)
-    # plt.show()
